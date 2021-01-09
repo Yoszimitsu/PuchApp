7868052adebb68e52c20ad8427ce0076758b7864
Krzysiek <kwilk@ittouch.com>
2021-01-09 13:46:57 +0100

    Spring init

---
+++ puch-app/src/test/java/pl/puch/puchapp/PuchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.puch.puchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuchAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
