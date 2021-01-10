6dc75ec9ce535b50131c9903244a592e43a131da
yoszimitsu <cotam07@gmail.com>
2021-01-10 17:21:17 +0100

    refactor

--- puch-app/src/main/java/pl/puch/puchapp/forest/dto/ForestRequestDto.java
+++ puch-app/src/main/java/pl/puch/puchapp/forest/dto/ForestRequestDto.java
@@ -5,15 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class ForestRequestDto {
 
-    @NotBlank(message = "ResourceId must not be blank.")
     private String resourceId;
     private String query;
     private String limit;
